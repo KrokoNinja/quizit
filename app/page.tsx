3bb4fc8f1eabf7a1ad4ed260c5b8368e3345383b
KrokoNinja <lenni.pfundtner@gmail.com>
2024-10-05 11:19:36 +0200

    Refactor page component and optimize image rendering

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,23 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function Home() {
-
+export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between p-24">
-      <div className='w-64'>
+      <div className="w-64">
         <AspectRatio ratio={238 / 261}>
-          <Image src={'/quizit-logo.png'} alt='QuizIT Logo' width={476} height={522} />
+          <Image
+            src={'/quizit-logo.png'}
+            alt="QuizIT Logo"
+            width={476}
+            height={522}
+          />
         </AspectRatio>
       </div>
-      <h1 className='text-primary'>QuizIT – People love IT</h1>
-      <Link href="/signup"><Button variant="tertiary">Get Started</Button></Link>
+      <h1 className="text-primary">QuizIT – People love IT</h1>
+      <Link href="/signup">
+        <Button variant="tertiary">Get Started</Button>
+      </Link>
     </div>
   );
 }
